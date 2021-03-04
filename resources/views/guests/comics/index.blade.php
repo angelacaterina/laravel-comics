79f6085d854db883098ab18f148aa3398ed1c99e
angelacaterina <angela.caterina@icloud.com>
2021-03-04 19:34:05 +0100

    comics card

--- resources/views/guests/comics/index.blade.php
+++ resources/views/guests/comics/index.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.guests.app')
 
 @section('content')
-   
+    <main id="main_content_comics">
+
+        <section id="comics_graphic_novels">
+            {{-- <div class="title">
+                <h2>COMICS & GRAPHIC NOVELS</h2>
+            </div> --}}
+            <div class="card_conteiner">
+                @foreach ($comics as $item)
+                    <div class="card_comics">
+                        <div class="comics">
+                            @if($item->cover)
+                                <a href="#"><img src="{{ asset('storage/' . $item->cover)}}" alt=""></a>
+                            @endif
+                        
+                            <div class="title_available">
+                                <p class="title_comics">{{ $item->title }}</p>
+                                <p class="available_comics">{{$item->available ? 'Available Now' : 'Unavailable'}}</p>
+                            </div>
+                        </div>
+                    </div> 
+                @endforeach           
+            </div>
+        </section>
+    </main>
 @endsection
--- resources/views/layouts/guests/head.blade.php
+++ resources/views/layouts/guests/head.blade.php
@@ -17,7 +17,8 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
-
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
