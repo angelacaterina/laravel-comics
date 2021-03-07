695cad0b69195fc9c47b6ae7b9135e8436601c28
angelacaterina <angela.caterina@icloud.com>
2021-03-07 17:42:02 +0100

    layout admin show collection

--- resources/views/admin/collections/show.blade.php
+++ resources/views/admin/collections/show.blade.php
@@ -1,11 +1,20 @@
 @extends('layouts.admin.dashboard')
 
 @section('content')
-    @if($collection->cover)
-    <img src="{{ asset('storage/' . $collection->cover)}}" alt="">
-    @endif
-    <h1>{{$collection->title}}</h1>
-    <h3>{{$collection->genre}}</h3>
 
-
+    <div class="show_admin">
+        <div class="card_show d-flex">
+            <div  class="cover">
+                @if($collection->cover)
+                    <img src="{{ asset('storage/' . $collection->cover)}}" alt="">
+                @endif
+            </div>
+        
+            <div class="card_text flex_column_center_center px-4">
+                <h1>{{$collection->title}}</h1>
+                <p>{{$collection->genre}}</p>
+            </div>
+        </div>
+    </div>
+   
 @endsection
--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.admin.dashboard')
 
 @section('content')
+
     <div class="show_admin">
         <div class="card_show">
             <div  class="jumbotron_img">
@@ -14,58 +15,63 @@
                 </div>
             </div>
           
-            <h1>
-                {{$comic->title}}
-                <span class="{{$comic->available ? 'green' : 'red'}}">
-                    <i class="fas fa-circle"></i>
-                </span>
-            </h1>
-            <p>{{$comic->description}}</p>
-        
-            <div class="card_info">
-                {{-- <p>Available: {{$comic->available ? 'si' : 'no'}}</p> --}}
-
-                <div class="info_content">
-                    <div class="name_info">Series:</div>
-                    <div class="body_info">{{$comic->collection ? $comic->collection->genre : 'N/A'}}</div>
+            <div class="card_row">
+                <div class="card_text col-xs-12 col-md-6 col-lg-6">
+                    <h1>
+                        {{$comic->title}}
+                        <span class="{{$comic->available ? 'green' : 'red'}}">
+                            <i class="fas fa-circle"></i>
+                        </span>
+                    </h1>
+                    <p>{{$comic->description}}</p>
                 </div>
-
-                <div class="info_content">
-                    <div class="name_info">Art by:</div>
-                    <div class="body_info"></div>
+    
+                <div class="card_info col-xs-12 col-md-6 col-lg-6">
+                    {{-- <p>Available: {{$comic->available ? 'si' : 'no'}}</p> --}}
+    
+                    <div class="info_content">
+                        <div class="name_info">Series:</div>
+                        <div class="body_info">{{$comic->collection ? $comic->collection->genre : 'N/A'}}</div>
+                    </div>
+    
+                    <div class="info_content">
+                        <div class="name_info">Art by:</div>
+                        <div class="body_info"></div>
+                    </div>
+    
+                    <div class="info_content">
+                        <div class="name_info">Written by:</div>
+                        <div class="body_info"></div>
+                    </div>
+    
+                    <div class="info_content">
+                        <div class="name_info">U.S. Price:</div>
+                        <div class="body_info">${{$comic->US_price}}</div>
+                    </div>
+                    <div class="info_content">
+                        <div class="name_info">On Sale Date:</div>
+                        <div class="body_info">{{$comic->on_sale_date}}</div>
+                    </div>
+                    <div class="info_content">
+                        <div class="name_info">Volume/Issue #:</div>
+                        <div class="body_info">{{$comic->volume_issue}}</div>
+                    </div>
+                    <div class="info_content">
+                        <div class="name_info">Trim Size:</div>
+                        <div class="body_info">{{$comic->trim_size}}</div>
+                    </div>
+                    <div class="info_content">
+                        <div class="name_info">Page Count:</div>
+                        <div class="body_info">{{$comic->page_count}}</div>
+                    </div>
+                    <div class="info_content">
+                        <div class="name_info">Rated:</div>
+                        <div class="body_info">{{$comic->rated}}</div>
+                    </div>
+    
                 </div>
-
-                <div class="info_content">
-                    <div class="name_info">Written by:</div>
-                    <div class="body_info"></div>
-                </div>
-
-                <div class="info_content">
-                    <div class="name_info">U.S. Price:</div>
-                    <div class="body_info">${{$comic->US_price}}</div>
-                </div>
-                <div class="info_content">
-                    <div class="name_info">On Sale Date:</div>
-                    <div class="body_info">{{$comic->on_sale_date}}</div>
-                </div>
-                <div class="info_content">
-                    <div class="name_info">Volume/Issue #:</div>
-                    <div class="body_info">{{$comic->volume_issue}}</div>
-                </div>
-                <div class="info_content">
-                    <div class="name_info">Trim Size:</div>
-                    <div class="body_info">{{$comic->trim_size}}</div>
-                </div>
-                <div class="info_content">
-                    <div class="name_info">Page Count:</div>
-                    <div class="body_info">{{$comic->page_count}}</div>
-                </div>
-                <div class="info_content">
-                    <div class="name_info">Rated:</div>
-                    <div class="body_info">{{$comic->rated}}</div>
-                </div>
-
             </div>
+            
         </div>
     </div>
     
