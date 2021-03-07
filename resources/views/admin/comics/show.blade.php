8a959e69b1bba68e42e8c0f01c37531f912a8407
angelacaterina <angela.caterina@icloud.com>
2021-03-07 15:17:53 +0100

    layout show admin

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -1,22 +1,72 @@
 @extends('layouts.admin.dashboard')
 
 @section('content')
-    @if($comic->cover)
-    <img src="{{ asset('storage/' . $comic->cover)}}" alt="">
-    @endif
-    @if($comic->jumbotron)
-    <img src="{{ asset('storage/' . $comic->jumbotron)}}" alt="">
-    @endif
-    <h1>{{$comic->title}}</h1>
-    <p>{{$comic->description}}</p>
+    <div class="show_admin">
+        <div class="card_show">
+            <div  class="jumbotron_img">
+                @if($comic->jumbotron)
+                    <img src="{{ asset('storage/' . $comic->jumbotron)}}" alt="">
+                @endif
+                <div  class="cover">
+                    @if($comic->cover)
+                        <img src="{{ asset('storage/' . $comic->cover)}}" alt="">
+                    @endif
+                </div>
+            </div>
+          
+            <h1>
+                {{$comic->title}}
+                <span class="{{$comic->available ? 'green' : 'red'}}">
+                    <i class="fas fa-circle"></i>
+                </span>
+            </h1>
+            <p>{{$comic->description}}</p>
+        
+            <div class="card_info">
+                {{-- <p>Available: {{$comic->available ? 'si' : 'no'}}</p> --}}
 
-    <p>Available: {{$comic->available ? 'si' : 'no'}}</p>
-    <p>US price: {{$comic->US_price}} $</p>
-    <p>On sale date: {{$comic->on_sale_date}}</p>
-    <p>Volume/Issue#: {{$comic->volume_issue}}</p>
-    <p>Trim size: {{$comic->trim_size}}</p>
-    <p>Page count: {{$comic->page_count}}</p>
-    <p>Rated: {{$comic->rated}}</p>
-    <p>Series: {{$comic->collection ? $comic->collection->genre : 'N/A'}}</p>
+                <div class="info_content">
+                    <div class="name_info">Series:</div>
+                    <div class="body_info">{{$comic->collection ? $comic->collection->genre : 'N/A'}}</div>
+                </div>
 
+                <div class="info_content">
+                    <div class="name_info">Art by:</div>
+                    <div class="body_info"></div>
+                </div>
+
+                <div class="info_content">
+                    <div class="name_info">Written by:</div>
+                    <div class="body_info"></div>
+                </div>
+
+                <div class="info_content">
+                    <div class="name_info">U.S. Price:</div>
+                    <div class="body_info">${{$comic->US_price}}</div>
+                </div>
+                <div class="info_content">
+                    <div class="name_info">On Sale Date:</div>
+                    <div class="body_info">{{$comic->on_sale_date}}</div>
+                </div>
+                <div class="info_content">
+                    <div class="name_info">Volume/Issue #:</div>
+                    <div class="body_info">{{$comic->volume_issue}}</div>
+                </div>
+                <div class="info_content">
+                    <div class="name_info">Trim Size:</div>
+                    <div class="body_info">{{$comic->trim_size}}</div>
+                </div>
+                <div class="info_content">
+                    <div class="name_info">Page Count:</div>
+                    <div class="body_info">{{$comic->page_count}}</div>
+                </div>
+                <div class="info_content">
+                    <div class="name_info">Rated:</div>
+                    <div class="body_info">{{$comic->rated}}</div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    
 @endsection
